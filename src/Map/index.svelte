188c3e2cc76b94a010d4670dbd7b1f2584851180
白熱 <sonne@asaki.me>
2023-08-08 00:36:36 +0800

    feat: add GeolocateControl plugin

--- src/Map/index.svelte
+++ src/Map/index.svelte
@@ -18,6 +18,15 @@ onMount(() => {
     style: mapController.mapboxStyleURL()
   })
   mapboxMap.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+  mapboxMap.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    // When active the map will receive updates to the device's location as it changes.
+    trackUserLocation: true,
+    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+    showUserHeading: true
+  }))
 
   mapboxMap.on('load', () => {
     mapController.registerMap(mapboxMap)
